eb60b34c56fb5d8d977789094e42789c3b525409
Adamantine-art <andreita.mosqueira@gmail.com>
2023-03-22 01:45:23 -0300

    Confirming path's existence Fn

--- api.js
+++ api.js
@@ -1,2 +1,9 @@
-// pequeñas fn
-// trabajar con fetch
+// Confirming the path's existence
+
+const fs = require('fs');
+
+const path = __dirname + '/README.md';
+
+if (fs.existsSync(path)) {
+    console.log('File exists :)');
+};
