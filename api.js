96dcb8158490d1cd3b4e34f384f1fc3ad39b26cd
Adamantine-art <andreita.mosqueira@gmail.com>
2023-03-27 20:32:18 -0300

    Adding the readFile function, setting new eslint rules

--- api.js
+++ api.js
@@ -1,8 +1,8 @@
-const fs = require('fs'); 
-const path = require('path'); 
+const fs = require('fs');
+const path = require('path');
 
 // Confirming the path's existence
-const pathExistence = (randomPath) => { // preguntar si los errores se pueden tomar solo con if else
+const pathExistence = (randomPath) => {
     if (fs.existsSync(randomPath)) {
         return true
     } else {
@@ -26,3 +26,18 @@ const isItMd = (randomPath) => {
     return path.extname(randomPath) === '.md';
 };
 console.log(isItMd('README.md'), 'is the file .md?');
+
+// Reading the Markdown file
+const readingTheFile = (randomPath) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(randomPath, 'utf-8', (error, fileContent) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(fileContent);
+            }
+        });
+        console.log('Mensaje para verificar si la función es asíncrona');
+    });
+};
+console.log(readingTheFile('README.md'), 'función asíncrona ejecutada');
