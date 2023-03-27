086dfaa073a6ee2665fe00253a44140888592e8a
Adamantine-art <andreita.mosqueira@gmail.com>
2023-03-26 23:04:43 -0300

    Adding API functions

--- api.js
+++ api.js
@@ -1,10 +1,28 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs'); 
+const path = require('path'); 
 
 // Confirming the path's existence
-const pathExistence = (randomPath) => fs.existsSync(randomPath)
-console.log(pathExistence('README.md'))
+const pathExistence = (randomPath) => { // preguntar si los errores se pueden tomar solo con if else
+    if (fs.existsSync(randomPath)) {
+        return true
+    } else {
+        return 'The path does not exist';
+    }
+};
+console.log(pathExistence('README.md'), 'does the path exist?');
 
 // Verifying if the path is absolute
 const isItAbsolute = (randomPath) => path.isAbsolute(randomPath)
-console.log(isItAbsolute('README.md'))
+console.log(isItAbsolute('README.md'), 'is it absolute?');
+
+// Converting the relative path into absolute
+const relativeToAbsolute = (randomPath) => {
+    return path.resolve(randomPath);
+};
+console.log(relativeToAbsolute('README.md'));
+
+// Checking the file's extension
+const isItMd = (randomPath) => {
+    return path.extname(randomPath) === '.md';
+};
+console.log(isItMd('README.md'), 'is the file .md?');
