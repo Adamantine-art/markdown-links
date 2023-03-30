90d8bd9bc4b674413f74b74d28a796497f2d46e5
Adamantine-art <andreita.mosqueira@gmail.com>
2023-03-30 01:50:04 -0300

    Finishing the Reading the file Function

--- api.js
+++ api.js
@@ -9,38 +9,43 @@ const pathExistence = (randomPath) => {
         return 'The path does not exist';
     }
 };
-console.log(pathExistence('README.md'), 'does the path exist?');
+console.log(pathExistence('README.md'), '<- does the path exist?');
 
 // Verifying if the path is absolute
 const isItAbsolute = (randomPath) => path.isAbsolute(randomPath)
-console.log(isItAbsolute('README.md'), 'is it absolute?');
+console.log(isItAbsolute('README.md'), '<- is it absolute?');
 
 // Converting the relative path into absolute
 const relativeToAbsolute = (randomPath) => {
     return path.resolve(randomPath);
 };
-console.log(relativeToAbsolute('README.md'));
+console.log(relativeToAbsolute('README.md'), '<- converting the relative path into absolute');
 
 // Checking the file's extension
 const isItMd = (randomPath) => {
     return path.extname(randomPath) === '.md';
 };
-console.log(isItMd('README.md'), 'is the file .md?');
+console.log(isItMd('README.md'), '<- is the file .md?');
 
 // Reading the markdown file
 const readingTheFile = (randomPath) => {
     return new Promise((resolve, reject) => {
-        fs.readFile(randomPath, 'utf-8', (error, fileContent) => { // estudiar el callback de este método
-            if (error) { // cambiar el if else por un try catch y tomar los errores correctamente
+        fs.readFile(randomPath, 'utf-8', (error, fileContent) => { 
+            if (error) { 
                 reject(error);
             } else {
                 resolve(fileContent);
             }
         });
-        console.log('Mensaje para verificar si la función es asíncrona'); // creo que esto no está funcionando asincrónico
     });
 };
-console.log(readingTheFile('README.md'), 'sigue el recorrido'); // por qué dice 'pending' siendo que ya le di una ruta?
+
+// Executing the promise
+readingTheFile('mdFile.md').then((mdContent) => {
+    console.log(mdContent);
+}).catch((err) => {
+    console.log(err);
+});
 
 // Getting the links within the file
 // RegExp
