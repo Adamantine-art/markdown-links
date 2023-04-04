0bd2e367a39e2168b32d9cf522c017b577af1d5a
Adamantine-art <andreita.mosqueira@gmail.com>
2023-04-04 17:45:41 -0400

    1st async function test finished, eslint & package json completed, starting the getting the links fn

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,32 +1,21 @@
-const { pathExistence, isItAbsolute, relativeToAbsolute, isItMd, readingTheFile } = require('../api.js');
+const { pathExistence, isItAbsolute, isItMd, readingTheFile } = require('../api.js');
 
 // Confirming the path's existence
 describe('Does the path exist?', () => {
-
   it('should return true', () => {
     expect(pathExistence('mdFile.md')).toBe(true);
   });
 });
 
-// Verifying if the path is absolute
-describe('Is the path absolute?', () => {
-
-  it('should return false', () => {
-    expect(isItAbsolute('mdFile.md')).toBe(false);
-  });
-});
-
 // Converting the relative path into absolute
 describe('Converting the relative path', () => {
-
   it('should convert into absolute', () => {
-    expect(relativeToAbsolute('mdFile.md')).toBe('C:\\Users\\Lenovo\\Desktop\\Laboratoria\\Markdown Links\\markdown-links\\mdFile.md');
+    expect(isItAbsolute('mdFile.md')).toBe('C:\\Users\\Lenovo\\Desktop\\Laboratoria\\Markdown Links\\markdown-links\\mdFile.md');
   });
 });
 
 // Checking the file's extension
 describe('Is the file .md?', () => {
-
   it('should return true', () => {
     expect(isItMd('mdFile.md')).toBe(true);
   });
@@ -34,8 +23,7 @@ describe('Is the file .md?', () => {
 
 // Reading the markdown file
 describe('Reading the .md file', () => {
-
   it('should read its content', () => {
-    expect(readingTheFile('mdFile.md')).toBe('✦ Reading the Markdown File ✦');
+    expect(readingTheFile('mdFile.md')).resolves.toBe('✦ Reading the Markdown File ✦')
   });
 });
